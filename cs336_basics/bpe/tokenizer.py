1b08f4b7620053ab22d1f12623b6b2b9f7aa4232
Tom <me@tomshen.io>
2026-03-04 21:35:22 -0800

    tokenizer done

--- cs336_basics/bpe/tokenizer.py
+++ cs336_basics/bpe/tokenizer.py
@@ -1,6 +1,13 @@
 from collections.abc import Generator, Iterable
-from cs336_basics.bpe.utils import BpeParameters, compile_special_token_patterns, split_into_pretokens
+from cs336_basics.bpe.utils import (
+    BpeParameters,
+    compile_special_token_patterns,
+    gpt2_bytes_to_unicode,
+    split_into_pretokens,
+)
 from collections import OrderedDict, deque
+import json
+
 
 class BpeTokenizer:
     def __init__(
@@ -9,7 +16,9 @@ class BpeTokenizer:
         self.vocab = vocab
         self.merges = merges
         self.word_to_token = {b: i for i, b in vocab.items()}
-        self.merges_order = {(self.word_to_token[left], self.word_to_token[right]): i for i, (left, right) in enumerate(merges)}
+        self.merges_order = {
+            (self.word_to_token[left], self.word_to_token[right]): i for i, (left, right) in enumerate(merges)
+        }
         self.special_tokens = special_tokens if special_tokens is not None else []
         self.pretoken_cache: OrderedDict[str, tuple[int, ...]] = OrderedDict()
         self.pretoken_cache_max_mize = 4096
@@ -19,31 +28,53 @@ class BpeTokenizer:
         return cls(params.vocab, params.merges)
 
     @classmethod
-    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens=None):
-        raise NotImplementedError()
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        # decode vocab from json
+        with open(vocab_filepath, mode="r", encoding="utf-8") as f:
+            vocab_inv: dict[bytes, int] = json.load(f)
+            vocab = {i: b for b, i in vocab_inv.items()}
+        gpt2_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
+        with open(merges_filepath, encoding="utf-8") as f:
+            merges = [tuple(line.rstrip().split(" ")) for line in f]
+            merges = [
+                (bytes([gpt2_decoder[token] for token in left]), bytes([gpt2_decoder[token] for token in right]))
+                for left, right in merges
+            ]
 
-    def to_files(self, vocab_filepath: str, merges_filepath: str, special_tokens=None):
-        raise NotImplementedError()
-    
+        return cls(vocab, merges, special_tokens)
+
+    def to_files(self, vocab_filepath: str, merges_filepath: str):
+        # encode vocab to json
+        vocabs_ser = {b.hex(): i for (i, b) in self.vocab.items()}
+        with open(vocab_filepath, mode="w", encoding="utf-8") as f:
+            json.dump(vocabs_ser, f)
+        # each merge is a line with gpt2_encoding(left_bytes) gpt2_encoding(right_bytes)
+        gpt2_encoder = gpt2_bytes_to_unicode()
+        with open(merges_filepath, mode="w", encoding="utf-8") as f:
+            f.writelines(
+                " ".join("".join(gpt2_encoder[b] for b in x) for x in (left, right)) for (left, right) in self.merges
+            )
+
     def encode(self, text: str) -> list[int]:
         return list(self.encode_chunk(text))
 
     def encode_chunk(self, text: str) -> Generator[int]:
         # pre tokenize the text
-        pretokens: list[tuple[int, ...]] = [] # each pretoken is represented by a tuple of token id in `self.vocab`
-        pretoken_to_idx: dict[str, int] = {} # token string -> pretokens id
-        text_in_pretokens_indices = [] # text represented by an ordered sequence of pretokens
+        pretokens: list[tuple[int, ...]] = []  # each pretoken is represented by a tuple of token id in `self.vocab`
+        pretoken_to_idx: dict[str, int] = {}  # token string -> pretokens id
+        text_in_pretokens_indices = []  # text represented by an ordered sequence of pretokens
         pretokens_text: list[str] = []
+
         def append_pretoken(pretoken: str, is_special: bool):
             if pretoken not in pretoken_to_idx:
-                    if pretoken in self.pretoken_cache:
-                        pretokens.append(self.pretoken_cache[pretoken])
-                    elif is_special:
-                        pretokens.append((self.word_to_token[pretoken.encode()],))
-                    else:
-                        pretokens.append(tuple(self.word_to_token[bytes([b])] for b in pretoken.encode()))
-                    pretokens_text.append(pretoken)
-                    pretoken_to_idx[pretoken] = len(pretokens) - 1
+                if pretoken in self.pretoken_cache:
+                    pretokens.append(self.pretoken_cache[pretoken])
+                elif is_special:
+                    pretokens.append((self.word_to_token[pretoken.encode()],))
+                else:
+                    pretokens.append(tuple(self.word_to_token[bytes([b])] for b in pretoken.encode()))
+                pretokens_text.append(pretoken)
+                pretoken_to_idx[pretoken] = len(pretokens) - 1
             text_in_pretokens_indices.append(pretoken_to_idx[pretoken])
 
         for lo, hi, is_special in split_into_pretokens(text, self.special_tokens):
@@ -58,9 +89,13 @@ class BpeTokenizer:
                     best_i = None
                     # find the lowest rank adjacent pair to merge, if any
                     for i in range(len(pretoken) - 1):
-                        left, right = pretoken[i], pretoken[i+1]
+                        left, right = pretoken[i], pretoken[i + 1]
                         if (left, right) in self.merges_order:
-                            if best_i is None or self.merges_order[(left, right)] < self.merges_order[(pretoken[best_i], pretoken[best_i + 1])]:
+                            if (
+                                best_i is None
+                                or self.merges_order[(left, right)]
+                                < self.merges_order[(pretoken[best_i], pretoken[best_i + 1])]
+                            ):
                                 best_i = i
                     # if no pair to merge, we are done!
                     if best_i is None:
@@ -68,7 +103,9 @@ class BpeTokenizer:
 
                     new_pretoken: list[int] = []
                     new_pretoken.extend(pretoken[j] for j in range(best_i))
-                    new_pretoken.append(self.word_to_token[self.vocab[pretoken[best_i]] + self.vocab[pretoken[best_i + 1]]])
+                    new_pretoken.append(
+                        self.word_to_token[self.vocab[pretoken[best_i]] + self.vocab[pretoken[best_i + 1]]]
+                    )
                     new_pretoken.extend(pretoken[j] for j in range(best_i + 2, len(pretoken)))
                     pretoken = tuple(new_pretoken)
                 pretokens[pretoken_idx] = pretoken
@@ -76,7 +113,7 @@ class BpeTokenizer:
                 if len(self.pretoken_cache) > self.pretoken_cache_max_mize:
                     self.pretoken_cache.popitem(last=False)
             else:
-                self.pretoken_cache.move_to_end(pretokens_text[pretoken_idx],last=True)
+                self.pretoken_cache.move_to_end(pretokens_text[pretoken_idx], last=True)
 
         for pretoken_idx in text_in_pretokens_indices:
             yield from pretokens[pretoken_idx]
@@ -84,11 +121,11 @@ class BpeTokenizer:
     def encode_iterable(self, iterable: Iterable[str]) -> Generator[int]:
         special_token_patterns = compile_special_token_patterns(self.special_tokens)
         buf = deque()
-        buf_release_threshold = 4 * 4096 * 4096 # when buffer size >= this threshold, 
-                                                # release the buffer before the beginning of next special token 
-                                                # that does not overlap on two string
-                                                # TODO: we could randomize this threshold to prevent malicious dataset putting all special token
-                                                # at boundaries, causing OOM
+        buf_release_threshold = 4 * 4096 * 4096  # when buffer size >= this threshold,
+        # release the buffer before the beginning of next special token
+        # that does not overlap on two string
+        # TODO: we could randomize this threshold to prevent malicious dataset putting all special token
+        # at boundaries, causing OOM
         stream = iter(iterable)
         while True:
             nxt = None
@@ -112,17 +149,13 @@ class BpeTokenizer:
                     yield from self.encode_chunk("".join(buf))
                     buf.clear()
                     buf.append(nxt[m:])
-            
+
             if nxt is None:
                 break
-        
+
         if len(buf) > 0:
             yield from self.encode_chunk("".join(buf))
             buf.clear()
-        
 
-                
-            
-
-            
-
+    def decode(self, ids: list[int]) -> str:
+        return b"".join(self.vocab[x] for x in ids).decode("utf-8", errors="replace")
